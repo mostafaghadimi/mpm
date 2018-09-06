f38fb9226e45b87560648c28855964278cbe7416
unknown <mostafa.ghadimi@yahoo.com>
2018-09-06 23:29:38 +0430

    transaction validation complete

--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
 
+var Product = require('../model/product');
+
 router.post('/validation', (req, res) => {
-    
+    var flag = false;
+    for (var i = 0; i < req.body.cart.length; i++){
+        Product.findOne({_id: req.body.cart[i]._id}).exec((err, result) => {
+            if (req.body.cart[i].count > result.count){
+                flag = true;
+                
+            }
+        })
+        if (flag) {
+            break;
+        }
+    }
+    if (flag) {
+        res.send({
+            // TODO: return unavailable goods
+            message: 'برخی از کالاها موجود نیستند',
+            success: false
+        })
+    }
+    else {
+        res.send({
+            message: 'سبد خرید تایید شد',
+            success: true,
+            information: req.body.cart
+        })
+    }
 })
 
+
+router.post('/done', (req, res) => {
+    // substract from the count
+    for (var i = 0; i < req.body.cart.length; i++){
+        Product.findOne({_id: req.body.cart[i]._id}).exec((err, result) => {
+                
+                
+            
+        })
+    }
+
+})
 
 module.exports = router;
