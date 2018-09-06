1bf437863fa195c32dc9a92c63ecd95ab9cf4d1f
unknown <mostafa.ghadimi@yahoo.com>
2018-09-07 01:54:07 +0430

    We hope everything works well :))

--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -2,47 +2,112 @@ var express = require('express');
 var router = express.Router();
 
 var Product = require('../model/product');
+var Transaction = require('../model/transaction');
+var User = require('../model/user');
 
 router.post('/validation', (req, res) => {
     var flag = false;
-    for (var i = 0; i < req.body.cart.length; i++){
-        Product.findOne({_id: req.body.cart[i]._id}).exec((err, result) => {
-            if (req.body.cart[i].count > result.count){
-                flag = true;
-                
+    for (var i = 0; i < req.body.cart.length; i++) {
+        Product.findOne({
+            _id: req.body.cart[i]._id
+        }).exec((err, result) => {
+            if (req.body.cart[i].count > result.count) {
+
+                return res.send({
+                    // TODO: return unavailable goods
+                    message: 'برخی از کالاها موجود نیستند',
+                    success: false
+                })
             }
         })
-        if (flag) {
-            break;
-        }
-    }
-    if (flag) {
-        res.send({
-            // TODO: return unavailable goods
-            message: 'برخی از کالاها موجود نیستند',
-            success: false
-        })
-    }
-    else {
-        res.send({
-            message: 'سبد خرید تایید شد',
-            success: true,
-            information: req.body.cart
-        })
-    }
-})
 
+    }
+
+    return res.send({
+        message: 'سبد خرید تایید شد',
+        success: true,
+        information: req.body.cart
+    })
+
+});
+
+router.post('/payment', (req, res) => {
+    var finalPrice = req.body.finalPrice;
+    var userID = req.body.userID;
+    User.findOne({
+        _id: userID
+    }, (err, user) => {
+        if (err || !user) {
+            res.send({
+                success: false,
+                message: 'کاربر نامعتبر است'
+            })
+        } else {
+            user.credit -= finalPrice;
+            user.save((err) => {
+                if (err) {
+                    res.send({
+                        success: false,
+                        message: 'خطا در به‌روز رسانی موجودی'
+                    })
+                }
+            })
+        }
+
+    })
+
+});
 
 router.post('/done', (req, res) => {
-    // substract from the count
-    for (var i = 0; i < req.body.cart.length; i++){
-        Product.findOne({_id: req.body.cart[i]._id}).exec((err, result) => {
-                
-                
-            
+    // TODO: push notification
+    for (var i = 0; i < req.body.cart.length; i++) {
+        var count = req.body.cart[i].count;
+        Product.findOne({
+            _id: req.body.cart[i]._id
+        }).exec((err, result) => {
+            if (err || !result) {
+                res.send({
+                    success: false,
+                    message: ' کالا نامعتبر است'
+                });
+            }
+            result.count -= count;
+            result.save((err) => {
+                if (err) {
+                    res.send({
+                        success: false,
+                        message: "خطا در ذخیره‌سازی"
+                    });
+                }
+            })
         })
     }
 
-})
+    var newTransaction = {
+        owner: req.body.userID,
+        cart: req.body.cart,
+        tokenDiscount: number(req.body.tokenDiscount)
+    }
+
+    Transaction.create(newTransaction, (err, result) => {
+        if (err) {
+            res.send({
+                message: 'خطا: تراکنش ثبت نشد',
+                success: false,
+            });
+        } else {
+            res.send({
+                message: 'تراکنش با موفقیت ثبت شد',
+                success: true
+            });
+        }
+    });
+
+    res.send({
+        message: 'خرید با موفقیت انجام شد',
+        success: true
+    });
+
+});
 
 module.exports = router;
