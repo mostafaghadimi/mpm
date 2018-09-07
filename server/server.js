614757003967a952d97d4148b6592aa7ecb8fee6
unknown <mostafa.ghadimi@yahoo.com>
2018-09-07 13:11:43 +0430

    payment page added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var app = express();
 
+var path = require('path');
+
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/marathon', {
   useNewUrlParser: true
@@ -12,7 +14,8 @@ db.once('open', function () {
   console.log('db connected!');
 });
 
-var morgan = require('morgan')
+var morgan = require('morgan');
+
 var session = require('express-session');
 app.use(session({
   secret: 'qpay.ir',
@@ -34,5 +37,12 @@ app.use('/user', users);
 
 var transaction = require('./routes/transactions');
 app.use('/transaction', transaction);
+
+app.use('/public', express.static(path.join(__dirname, '../public')));
+app.use('/pay', express.static(path.join(__dirname, '../public/html/pay/')))
+
+app.get('/payment', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/html/ipg.html'))
+})
 
 app.listen(8080, console.log('server connected: 8080'))
